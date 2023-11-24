b9eff56d162876f0368c65c27ce33ff6590d4f70
LehaVredniy <145477806+LehaVredniy@users.noreply.github.com>
2023-11-24 22:25:27 +0300

    .\Task_66_SumNum\

---
+++ task_66_SumNum/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая найдет сумму натуральных элементов
+// в промежутке от M до N с помощью рекурсии.
+// M = 1, N = 15 -> 120;
+// M = 4, N = 8 -> 30;
+
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int m, int n)
+{
+    if(m == n)
+    {
+        return n;
+    }
+    else return m + SumNumbers(m +1,n);
+}
+Console.Write(SumNumbers(m,n));
+//SumNumbers(m,n);
+

b2b4c7005f529ab3d912d766427090504b35b4e5
LehaVredniy <145477806+LehaVredniy@users.noreply.github.com>
2023-11-24 19:13:53 +0300

    task

--- task_64/Program.cs
+++ task_64/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите число M");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int num = 1;
+if (num <= num2)
+{
+    if (num % 2 == 0)
+    {
+      Console.WriteLine( num + ", ");  
+    }
+    count++;
+}
+
+// PrintEvenNumbers(num1, num2);
+
+// void PrintEvenNumbers(int start, int and)
+// {
+//     Console.Write(start + ", ");
+//     if(start == and)
+//     { 
+//         return;
+//     } 
+//     else if(start % 2 == 0)
+//     {
+//         PrintEvenNumbers(start +1, and);
+//     }  
+// }
+
